db0fb8ef9879fe39ba29a4bfafd00bcbe1bbf8bb
Ana Carolina <anacarolinamb2@gmail.com>
2023-09-08 22:01:51 -0300

    retirada do card

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -9,25 +9,9 @@ export function Home() {
             <h1 id='tituloHome'>WeightPlanner</h1>
             <div id='linha' > </div>
             <div className='ctHome'>
-            <p id='textoPrincipal' >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit harum, optio maxime non nobis error est quod quos tempora eveniet sit totam minus numquam quaerat nam a repellendus exercitationem beatae?</p>
-            <div className="dbalanca"> <img src={Dbalanca} alt="" /> </div>
-            <div className="containerCard">
-                <div id="linhaDireita"></div>
-                <div className="girar-card">
-                    <div className="girar-card-frente">
-                        <div className="frente-card">
-                            <p className="textoCard">Precisa de agilidade e praticidade ao pesar?</p>
-                            {/* <p>Hover Me</p> */}
-                        </div>
-                        <div className="atras-card">
-                            <p className="textoCard">Então a WeightPlanner é perfeita para você!</p>
-                            {/* <p>Leave Me</p> */}
-                        </div>
-                    </div>
-                </div>
-                <div id="linhaDireita"></div>
+                <p id='textoPrincipal'>Com a WeightPlanner, a balança da atualidade, sua vida se torna muito mais fácil. Com a possibilidade de conectar suas pesagens direto ao Excel, é possível organizar sua vida com um clique!</p>
+                <div className="dbalanca"> <img src={Dbalanca} alt="" /> </div>
             </div>
-        </div>
         </div>
     )
 }
