5a88b8f18d09c64831ac7af9d4741cfdd6edf322
Beatriz <beatriznovais1404@gmail.com>
2023-09-28 13:06:06 -0300

    finalizacao pagina objetivo

--- src/pages/Objetivo/index.jsx
+++ src/pages/Objetivo/index.jsx
@@ -3,18 +3,27 @@ import ImgObjetivo from "../../assets/imagemObjetivo.svg"
 
 export function Objetivo () {
     return (
-        <body className='ObjetivoConteudo'>
+        <div className='ObjetivoConteudo'>
             <h1 id='tituloObjetivo'>Nosso Objetivo</h1>
-            <div className='caixa_ct1'>
-                <p>Temos como objetivo facilitar a organização de pesagens em grande ou pequena escala.</p>
+            <div className="content">
+            <div id='principal'>
+                <div className='caixa_ct1'>
+                    <p>Temos como objetivo facilitar a organização de pesagens em grande ou pequena escala.</p>
+                </div>
+                <div className='caixa_img'>
+                    <img src= {ImgObjetivo} alt="" />
+                </div>
+                <div className='caixa_ct2'>
+                    <p>Ajudando pessoas a regrarem melhor suas alimentações e assim terem uma melhor relação com a comida. Além das demais funcionalidades que a balança pode ter.</p>
+                </div>
             </div>
-            <div className='caixa_img'>
+            <div className='caixa_img_mobile'>
                 <img src= {ImgObjetivo} alt="" />
             </div>
-            <div className='caixa_ct2'>
-                <p>Ajudando pessoas a regrarem melhor suas alimentações e assim terem uma melhor relação com a comida. Além das demais funcionalidades que a balança pode ter.</p>
+
             </div>
 
-        </body>
+
+        </div>
     )
 }
