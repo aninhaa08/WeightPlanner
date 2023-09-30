afadd57041c3e5cfc362dec3d1f0957251813e45
Ana Carolina <anacarolinamb2@gmail.com>
2023-09-30 16:37:57 -0300

    ajustes basicos rodape

--- src/pages/Objetivo/index.jsx
+++ src/pages/Objetivo/index.jsx
@@ -1,29 +1,28 @@
 import './style.scss'
 import ImgObjetivo from "../../assets/imagemObjetivo.svg"
+import Rodape from '../../components/Rodape'
 
 export function Objetivo () {
     return (
         <div className='ObjetivoConteudo'>
             <h1 id='tituloObjetivo'>Nosso Objetivo</h1>
             <div className="contentObjetivo">
-            <div id='principal'>
-                <div className='caixa_ct1'>
-                    <p>Temos como objetivo facilitar a organização de pesagens em grande ou pequena escala.</p>
+                <div id='principal'>
+                    <div className='caixa_ct1'>
+                        <p>Temos como objetivo facilitar a organização de pesagens em grande ou pequena escala.</p>
+                    </div>
+                    <div className='caixa_img'>
+                        <img src= {ImgObjetivo} alt="" />
+                    </div>
+                    <div className='caixa_ct2'>
+                        <p>Ajudando pessoas a regrarem melhor suas alimentações e assim terem uma melhor relação com a comida. Além das demais funcionalidades que a balança pode ter.</p>
+                    </div>
                 </div>
-                <div className='caixa_img'>
+                <div className='caixa_img_mobile'>
                     <img src= {ImgObjetivo} alt="" />
                 </div>
-                <div className='caixa_ct2'>
-                    <p>Ajudando pessoas a regrarem melhor suas alimentações e assim terem uma melhor relação com a comida. Além das demais funcionalidades que a balança pode ter.</p>
-                </div>
             </div>
-            <div className='caixa_img_mobile'>
-                <img src= {ImgObjetivo} alt="" />
-            </div>
-
-            </div>
-
-
+            <Rodape />
         </div>
     )
 }
