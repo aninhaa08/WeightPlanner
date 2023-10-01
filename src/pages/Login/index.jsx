1fd5776f25522db1e4d75545cfb5b2cfb48fd8d1
Ana Carolina <anacarolinamb2@gmail.com>
2023-10-01 04:18:17 -0300

    ajustes page login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,27 +1,56 @@
 import './style.scss'
+import logo from '../../assets/logoWebBranco.png'
 
 export function Login() {
     return(
         <div className="loginConteudo">
-            <h1 id="tituloLogin">Bem-vindo de volta</h1>
-            <div id="linhaLogin"></div>
-            <div className="caixaLogin">
-                <p id="subtituloLogin">Login</p>
-                <form id="formLogin" action="">
-                    <div className="input" id="input1">
-                        <input className="inputForm" id="email" type="email" placeholder="Email" />
-                        <div className="detalheInput1"></div>
-                    </div>
-                    <div className="input" id="input2">
-                        <input className="inputForm" id="senha" type="password" placeholder="Senha" />
-                        <div className="detalheInput2"></div>
-                    </div>
-                </form>
+            <div className="loginMobile">
+                <h1 id="tituloLogin">Bem-vindo de volta</h1>
+                <div id="linhaLogin"></div>
+                <div className="caixaLogin">
+                    <p id="subtituloLogin">Login</p>
+                    <form id="formLogin" action="">
+                        <div className="input" id="input1">
+                            <input className="inputForm" id="email" type="email" placeholder="Email" />
+                            <div></div>
+                        </div>
+                        <div className="input" id="input2">
+                            <input className="inputForm" id="senha" type="password" placeholder="Senha" />
+                            <div></div>
+                        </div>
+                    </form>
+                </div>
+                <button id="botaoLogin">Logar</button>
+                <div className="caixaSemConta">
+                    <p>Ainda não possui uma conta?</p>
+                    <a href="/cadastro">Cadastre-se aqui</a>
+                </div>
             </div>
-            <button id="botaoLogin">Logar</button>
-            <div className="caixaSemConta">
-                <p>Ainda não possui uma conta?</p>
-                <a href="/cadastro">Cadastre-se aqui</a>
+            <div className="loginWeb">
+                <div className="caixaMaior">
+                    <div className="caixaMenor">
+                        <img src={logo} alt="" />
+                        <h1 id="tituloLoginW">Bem-vindo de volta</h1>
+                        <div id="linhaLoginW"></div>
+                        <div className="caixaSemContaW">
+                            <p>Ainda não possui uma conta?</p>
+                            <a href="/cadastro">Cadastre-se aqui</a>
+                        </div>
+                    </div>
+                    <div className="caixaLoginW">
+                        <p id="subtituloLoginW">Login</p>
+                        <form id="formLoginW" action="">
+                            <div className="input" id="input1">
+                                <input className="inputFormW" id="email" type="email" placeholder="Email" />
+                                <div></div>
+                            </div>
+                            <div className="input" id="input2">
+                                <input className="inputFormW" id="senha" type="password" placeholder="Senha" />
+                                <div></div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     )
