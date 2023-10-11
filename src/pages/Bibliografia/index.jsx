81f1a2e0ad9318370dd651a9f3a8d7bc4032520d
Ana Carolina <anacarolinamb2@gmail.com>
2023-10-11 18:22:20 -0300

    ajustes page ref/obj/cad/login

--- src/pages/Bibliografia/index.jsx
+++ src/pages/Bibliografia/index.jsx
@@ -7,8 +7,10 @@ export function Bibliografia () {
         <div className='refeConteudo'>
             <div className='refe_principal'>
                 <h1 id="tituloRefe">Bibliografia</h1>
-                <div className="refe_caixa">
-                    <p>Procuramos diversos projetos semelhantes como inspiração para a balança WeightPlanner e a seguir estão os que mais nos ajudaram e serviram de base para o projeto.</p>
+                <div className="ctPrincipal">
+                    <div className="refe_caixa">
+                        <p>Procuramos diversos projetos semelhantes como inspiração para a balança WeightPlanner e a seguir estão os que mais nos ajudaram e serviram de base para o projeto.</p>
+                    </div>
                 </div>
                 <h3 className='refe_subtitulo'>Referências <br className='quebra_linha_refe'/> bibliográficas</h3>
                 <div className="container_refe">
@@ -25,16 +27,16 @@ export function Bibliografia () {
                         <p>Youtube</p>
                     </div>
                     <div className="content_refe">
-                        <a href="https://www.youtube.com/watch?v=V8PHw3e-aPo&ab_channel=BrincandocomIdeias" target="_blank">
-                            <img src= {ImgReferencias} alt="" />
-                        </a>
-                        <p>Youtube</p>
-                    </div>
-                    <div className="content_refe">
                         <a href="https://blog.eletrogate.com/guia-completo-do-arduino-leonardo/" target="_blank">
                             <img src= {ImgReferencias} alt="" />
                         </a>
                         <p>Blog</p>
+                    </div>
+                    <div className="content_refe">
+                        <a href="https://www.youtube.com/watch?v=V8PHw3e-aPo&ab_channel=BrincandocomIdeias" target="_blank">
+                            <img src= {ImgReferencias} alt="" />
+                        </a>
+                        <p>Youtube</p>
                     </div>
                 </div>
                 <div className='mao_robo_refe'>
